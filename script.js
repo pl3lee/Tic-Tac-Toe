f1814cd5a93be155db0c2f8f4bf837c6434778e2
pl3lee <billylee123@gmail.com>
2022-05-06 13:24:22 -0400

    created the Gameboard module

---
+++ script.js
@@ -0,0 +1,50 @@
+const Gameboard = (() => {
+    let grid = ['B', 'B', 'B',
+                'B', 'B', 'B',
+                'B', 'B', 'B'];
+    const makeMove = (player, position) => {
+        if (grid[position] == 'B') {
+            grid[position] = player;
+            return true;
+        } else {
+            return false;
+        }
+    };
+    const checkFull = () => !grid.includes('B');
+    const checkWin = () => {
+        // check rows
+        if ((grid[0] == grid[1]) && (grid[1] == grid[2]) && (grid[2] != 'B')) {
+            return grid[0];
+        }
+        if ((grid[3] == grid[4]) && (grid[4] == grid[5]) && (grid[5] != 'B')) {
+            return grid[3];
+        }
+        if ((grid[6] == grid[7]) && (grid[7] == grid[8]) && (grid[8] != 'B')) {
+            return grid[6];
+        }
+
+        // check cols
+        if ((grid[0] == grid[3]) && (grid[3] == grid[6]) && (grid[6] != 'B')) {
+            return grid[0];
+        }
+        if ((grid[1] == grid[4]) && (grid[4] == grid[7]) && (grid[7] != 'B')) {
+            return grid[1];
+        }
+        if ((grid[2] == grid[5]) && (grid[5] == grid[8]) && (grid[8] != 'B')) {
+            return grid[1];
+        }
+
+        //check diagonals
+        if ((grid[0] == grid[4]) && (grid[4] == grid[8]) && (grid[8] != 'B')) {
+            return grid[0];
+        }
+        if ((grid[2] == grid[4]) && (grid[4] == grid[6]) && (grid[6] != 'B')) {
+            return grid[2];
+        }
+        
+        return false;
+    };
+    const checkTie = () => !checkWin() && checkFull();
+    const getPosition = (position) => grid[position];
+})();
+
